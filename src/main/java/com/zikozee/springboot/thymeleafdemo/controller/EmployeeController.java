fa4e31135d3a992b19067701643555308b28eda7
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2019-12-30 16:02:26 +0100

    refactored

--- src/main/java/com/zikozee/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/zikozee/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,14 +4,20 @@ import com.zikozee.springboot.thymeleafdemo.entity.Employee;
 import com.zikozee.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class EmployeeController {
 
    private EmployeeService employeeService;
+
+   private Map<String, String> budgetChooser;
 
    //since we have one service autowired is optional , else we need specify Autowired(value= "theService")
    public EmployeeController(EmployeeService theEmployeeService){
@@ -73,5 +79,6 @@ public class EmployeeController {
         // use a redirect to prevent duplicate submissions
         return "redirect:/";
     }
+
 
 }
